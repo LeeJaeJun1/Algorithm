33ea066880afafcd96bc4355b3ed123f507e4da0
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2025-02-25 14:11:34 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2408 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -5,26 +5,24 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int count = 0;
-    stack<int> sta;
-    string s; cin >> s;
 
-    for(int i = 0; i < s.size(); i++) {
-        if(s[i] == '(' && s[i+1] == ')') {
-            // 레이저 발사. 그 전에 담겨진 stack 개수 합친다.
-            count += sta.size();
+    stack<int> s;
+    string str;
+    int count = 0;
+    cin >> str;
+
+    for(int i = 0; i < str.size(); i++) {
+        if(str[i] == '(' && str[i+1] == ')') {
+            count += s.size();
             i += 1;
         }
-        else if(s[i] == '(') {
-            // 스택에 담는다.
-            sta.push(i);
+        else if(str[i] == '(') {
+            s.push(i);
         }
-        else if(s[i] == ')') {
-            // 바로 직전의 (와 현재 )가 만들어낸 막대기 하나 추가하고 바로 직전 ( pop한다. 
+        else if(str[i] == ')') {
             count += 1;
-            sta.pop();
+            s.pop();
         }
     }
     cout << count << endl;
 }
-
