0826b6dc1f7a2feb51a801d77a06af2cd2009a3b
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2025-03-06 12:29:27 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,22 +1,24 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 
+// N에서 3을 빼면서, 5의 배수가 되면 나눈다.
 int main() {
-    int N;
-    cin >> N;
-    int answer{0};
-    
-    while(N >= 0) {
-        if(N % 5 == 0) { // N이 5의 배수이면 몫이 필요한 봉지개수이다.
-            answer += N / 5;
-            cout << answer << endl;
-            return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int total = 0;
+    while(true) {
+        if(n % 5 == 0) {
+            total += n / 5;
+            break;
         }
-        else{
-        N -= 3; // 5의 배수가 아니면 3씩 빼주고,총 봉지개수 추가
-        answer++;
+        else if(n < 0) {
+            total = -1;
+            break;
         }
+        n-=3;
+        total++;
     }
-    cout << "-1" << endl; // 정확한 N킬로그램 만들 수 없다면 -1 출력
+    cout<< total << endl;
 }
