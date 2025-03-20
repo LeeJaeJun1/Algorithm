aa7e0646b0e8fd0e86958bf444da192fc12110a7
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2025-03-20 12:45:50 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,56 +1,85 @@
 #include<iostream>
-#include<queue>
 using namespace std;
 
-int main() {
-    int N{0};
-    string command;
-    queue<int> q;
-    
-    cin >> N;
-    
-    for(int i = 0; i < N; i++) {
-        cin >> command;
+class arrayQueue{
+private:
+    int front;
+    int rear;
+    int* arr;
+    int capacity;
+    int size;
+public:
+    arrayQueue() {
+        capacity = 10000;
+        front = rear = 0;
+        size = 0;
+        arr = new int[capacity];
+    }
+    bool empty() {
+        return size==0;
+    }
+    int Front() {
+        if(empty()) {
+            return -1;
+        }
+        return arr[front];
+    }
+    int Back() {
+        if(empty()) {
+            return -1;
+        }
+        return arr[rear-1];
+    }
+    int Size() {
+        return size;
+    }
+    void push(int x) {
+        arr[rear] = x;
+        rear = (rear+1) % capacity;
+        size++;
+    }
+    void pop() {
+        if(empty()) {
+            cout << -1 << "\n";
+            return;
+        }
+        cout << arr[front] << "\n";
+        front = (front+1) % capacity;
+        size--;
+    }
+};
 
-        if(command == "push") {
-            int n;
-            cin >> n;
-            q.push(n);
-        }
-        
-        else if(command == "front") {
-            if(q.size() == 0) cout << "-1" << "\n";
-            else {
-                cout << q.front() << "\n";
-            }
-        }
-        
-        else if(command == "back") {
-            if(q.size() == 0) cout << "-1" << "\n";
-            else {
-                cout << q.back() << "\n";
-            }
-        }
-        
-        else if(command == "size") {
-            cout << q.size() << "\n";
-        }
-        
-        else if(command == "empty") {
-            if(q.size() == 0) {
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n; string s;
+    cin >> n;
+    arrayQueue queue;
+    while(n--) {
+        cin >> s;
+        if(s=="empty") {
+            if(queue.empty()) {
                 cout << "1" << "\n";
             }
-            else cout << "0" << "\n";
-        }
-        
-        else if(command == "pop") {
-            if(q.size() == 0) cout << "-1" << "\n";
-            else {
-                cout << q.front() << "\n";
-                q.pop();
+            else{
+                cout << "0" << "\n";
             }
         }
+        else if(s=="size") {
+            cout << queue.Size() << "\n";
+        }
+        else if(s=="front") {
+            cout << queue.Front() << "\n";
+        }
+        else if(s=="back") {
+            cout << queue.Back() << "\n";
+        }
+        else if(s=="push") {
+            int x; cin >> x;
+            queue.push(x);
+        }
+        else if(s=="pop") {
+            queue.pop();
+        }
     }
-    
-    return 0;
 }
