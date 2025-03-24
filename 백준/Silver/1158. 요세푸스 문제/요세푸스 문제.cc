f55511fe0e6051a3f20a871f71004ac7244a8f73
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2025-03-24 12:48:04 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 48 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -3,25 +3,26 @@
 using namespace std;
 
 int main() {
-    int N, K;
-    queue<int> Q;
-    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N,K,fro;
+    queue<int> qu;
     cin >> N >> K;
-    
     for(int i = 1; i <= N; i++) {
-        Q.push(i);
+        qu.push(i);
     }
-    
     cout << "<";
-    
-    while(Q.size() > 1) {
-    for(int i = 1; i < K; i++) {
-        int num = Q.front();
-        Q.pop();
-        Q.push(num);
+    while(!qu.empty()) {
+        for(int i = 1; i < K; i++) {
+            fro = qu.front();
+            qu.pop();
+            qu.push(fro);
+        }
+        if(qu.size() == 1) {
+            cout << qu.front() << ">";
+            break;
+        }
+        cout << qu.front() << ", ";
+        qu.pop();
     }
-    cout << Q.front() << ", ";
-    Q.pop();
-  }
-  cout << Q.front() << ">";
 }
