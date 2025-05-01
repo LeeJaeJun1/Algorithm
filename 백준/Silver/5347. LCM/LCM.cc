12d1a0b906b1fd22c5e66ea88e4b1ecf118b3d26
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2025-05-01 22:42:54 +0900

    [Silver V] Title: LCM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/5347. LCM/LCM.cc
+++ 백준/Silver/5347. LCM/LCM.cc
@@ -1,26 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int check(long a, long b) {
+int gcd(long a, long b) {
     if(a % b == 0) {
         return b;
     }
-    return check(b, a % b);
+    return gcd(b, a % b);
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    long n,a,b,ta,tb; cin >> n;
-    long total = 0;
-
-    while(n--) {
+    long test,a,b;
+    cin >> test;
+    while(test--) {
         cin >> a >> b;
-        ta = (a / check(a,b));
-        tb = (b / check(a,b));
-        total = check(a,b) * ta * tb;
-        cout << total << endl;
+        cout << (a / gcd(a,b)) * (b / gcd(a,b)) * gcd(a,b) << "\n";
     }
-    return 0;
 }
