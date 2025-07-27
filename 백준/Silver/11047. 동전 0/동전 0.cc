8552bed96533c8330054aa694ace47f14f33d04c
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2025-07-27 11:43:02 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -7,11 +7,11 @@ int main() {
     int n,k,num = 0;
     cin >> n >> k;
     int arr1[n];
-    
+
     for(int i = 0; i < n; i++) {
         cin >> arr1[i];
     }
-    
+
     for(int j = n - 1; j >= 0; j--) {
         if(k >= arr1[j]) {
             num += k / arr1[j];
