0720440ba9ffcc674853a7fb3c1037aabb458a12
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2025-08-20 10:21:45 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+
+    // 2 x n 크기의 직사각형 1x2, 2x1로 채우는 방법의 수
+    // 2xn 크기의 직사각형 채우는 방법의 수 10,007로 나눈 나머지
+
+    int n; int arr[1001];
+    arr[1] = 1; arr[2] = 2;
+    cin >> n;
+
+
+    for(int i = 3; i <= n; i++) {
+        arr[i] = (arr[i-1] + arr[i-2]) % 10007;
+    }
+    cout << arr[n] << "\n";
+}
