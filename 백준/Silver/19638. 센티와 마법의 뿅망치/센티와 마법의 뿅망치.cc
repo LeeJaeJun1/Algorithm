65ee7599575f95a2d1640e3d7b6b1b14e31d61a3
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2025-09-02 12:35:07 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 24 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.cc
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.cc
@@ -1,54 +1,39 @@
 #include<iostream>
 #include<queue>
-#include<algorithm>
 using namespace std;
-
-// 맞은 사람 키가 l 키 / 2 , 1인경우 영향 안받음
-// 가장 키가 큰 거인 중 한 명 때린다
-// 인구수와 센티의 키 횟수 제한
-// 거인의 키
-// 모든 거인이 센티보다 키가 작도록 YES, 최소 횟수
-// 거인의 나라에서 센티보다 키가 크거나 같은 거인 NO, 키가 가장 큰 거인
-// 1일 때는 영향 미치지 않는다.
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
 
+    // 가장 키가 큰 거인 가운데 하나를 때린다.
+    // 뿅먕치 맞은 사람의 키 / 2로 변함
     priority_queue<int> pq;
+    long long num,senti,test,h,count = 0;
+    cin >> num >> senti >> test;
 
-    int n, cenH, limit,height,count = 0;
-
-
-    cin >> n >> cenH >> limit;
-
-    for(int i = 0; i < n; i++) {
-        cin >> height;
-        pq.push(height);
+    for(int i = 0; i < num; i++) {
+        cin >> h;
+        pq.push(h);
     }
 
-    for(int j = 0; j < limit; j++) {
-        if(pq.top() < cenH) {
+    while(test--) {
+        int t = pq.top();
+        if(t >= senti && t != 1) {
+            pq.pop();
+            pq.push(t/2);
+            count++;
+        }
+        else {
             break;
         }
-        int cur = pq.top();
-        if(cur == 1) {
-            break;
-        }
-        cur /= 2;
-        pq.pop();
-        pq.push(cur);
-        count++;
     }
 
-    if(pq.top() >= cenH) {
+    if(pq.top() >= senti) {
         cout << "NO\n" << pq.top();
     }
     else {
         cout << "YES\n" << count;
     }
-
-
-    // 가장 큰 사람부터 뿅망치 맞추고
-    // 32 16  / 32/2 = 16 , 16
 }
