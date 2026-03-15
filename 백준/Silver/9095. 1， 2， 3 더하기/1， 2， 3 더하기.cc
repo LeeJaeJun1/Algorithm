5d9b9088f1be48f55d042f10d0561a9c59fe8c98
SmileJun <108278044+LeeJaeJun1@users.noreply.github.com>
2026-03-15 22:35:35 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -2,15 +2,18 @@
 using namespace std;
 
 int main() {
-    int arr[20];
-    arr[1] = 1; arr[2] = 2; arr[3] = 4;
-    int test,n;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int test, n, arr[12];
+    arr[1] = 1, arr[2] = 2, arr[3] = 4;
     cin >> test;
-    while(test--) {
-        cin >> n;
-        for(int i = 4; i <= n; i++) {
+    while (test--) {
+        cin >> n; // 양수이며 11보다 작다
+        for (int i = 4; i <= n; i++) {
             arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
         }
-        cout << arr[n] << "\n";
+       cout << arr[n] << "\n";
     }
 }
